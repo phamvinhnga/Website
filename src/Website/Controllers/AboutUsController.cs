41e988673b9a1f0c4f7181a85f973ba09cb2108d
nga.vinhpham <101375733+ngaphaminfodation@users.noreply.github.com>
2023-04-04 00:29:11 +0700

    xxxx

---
+++ src/Website/Controllers/AboutUsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Website.Entity.Repositories.Interfaces;
+using Website.Entity;
+
+namespace Website.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
+        private readonly IPostRepository _postRepository;
+
+        public AboutUsController(
+            ILogger<HomeController> logger,
+            IPostRepository postRepository,
+            ApplicationDbContext context
+        )
+        {
+            _logger = logger;
+            _context = context;
+            _postRepository = postRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var p = await _postRepository.GetByIdAsync(1);
+            ViewBag.MetaTitle = p.MetaTitle;
+            ViewBag.Permalink = p.Permalink;
+            ViewBag.MetaDescription = p.MetaDescription;
+            ViewBag.Content = p.Content;
+            return View(p);
+        }
+    }
+}
--- src/Website/Controllers/HomeController.cs
+++ src/Website/Controllers/HomeController.cs
@@ -33,11 +33,6 @@ namespace Website.Controllers
             return View(p);
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
