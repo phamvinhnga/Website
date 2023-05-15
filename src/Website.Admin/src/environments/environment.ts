596691610db8082a1ca09711a7055cee434fe9cd
nga.vinhpham <40154445+phamvinhnga@users.noreply.github.com>
2023-05-15 16:20:19 +0700

    .....

--- src/Website.Admin/src/environments/environment.ts
+++ src/Website.Admin/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   websiteURL: 'http://huyenduc.click',
-  coreServerURL: 'http://api.huyenduc.click'
+  coreServerURL: 'http://localhost:5000'
 };
 
