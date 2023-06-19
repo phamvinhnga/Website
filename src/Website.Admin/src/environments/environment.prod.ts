fd85ce7d81951c6a54f4fff5bbcb0ead1ab11f94
nga.vinhpham <101375733+ngaphaminfodation@users.noreply.github.com>
2023-06-19 16:39:14 +0700

    .

--- src/Website.Admin/src/environments/environment.prod.ts
+++ src/Website.Admin/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   websiteURL: 'http://huyenduc.click',
-  coreServerURL: 'http://163.44.206.25:5000'
+  coreServerURL: ''
 };
 
--- src/Website.Admin/src/environments/environment.ts
+++ src/Website.Admin/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   websiteURL: 'http://huyenduc.click',
-  coreServerURL: 'http://163.44.206.25:5000'
+  coreServerURL: ''
 };
 
